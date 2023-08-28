fbb530bf2e07d95035c46a7af9f4e5c430122e7a
ka1sser <serenokaisser@gmail.com>
2023-08-28 15:48:14 +0800

    Visual effect for hadPackage and noPackage status

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Delivery : MonoBehaviour
 {
-    //default value is false
-    bool hasPackage;
+    [SerializeField] Color32 hasPackageColor = new Color32(255,125,0,255);
+    [SerializeField] Color32 noPackageColor = new Color32(0,0,0,0);
+
+   
+    bool hasPackage;  //default bool value is false
     [SerializeField] float destroyTime=1f;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Awts!");
@@ -18,11 +30,14 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyTime);
+            
         }
         if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Homie!");
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
     }
